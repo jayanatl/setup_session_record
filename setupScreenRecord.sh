1f71ffa52b1eeabc1c3ed8bbde43202cf8a1c22d
Jayan Brahmanandan <jayan.brahmanandan@reancloud.com>
2018-04-25 13:30:19 +0530

    add download start_recording

--- setupScreenRecord.sh
+++ setupScreenRecord.sh
@@ -14,6 +14,8 @@ cp terminal_web_player/view.* /var/www/html/${web_loc}
 wget https://raw.github.com/chjj/tty.js/d379c6f9/static/term.js -o /var/www/html/view/term.js
 mkdir -p ~/workshop/{bin,sessions}
 grep 'PATH=$PATH:~/workshop/bin' ~/.bashrc || echo "export PATH=$PATH:~/workshop/bin" >> ~/.bashrc
-
+export PATH=$PATH:~/workshop/bin
+wget https://raw.githubusercontent.com/jayanatl/setup_session_record/master/start_recording -o ~/workshop/bin/start_recording
+chmod u+x ~/workshop/bin/start_recording
 systemctl enable httpd
 systemctl start httpd
