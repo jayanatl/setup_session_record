9445d439eb729a7d58c8d64c8c0ca5a1f400b010
Jayan Brahmanandan <jayan.brahmanandan@reancloud.com>
2018-04-25 13:21:49 +0530

    Add script skels

---
+++ setupScreenRecord.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ ${EUID} eq 0 ]; then
+    echo "Error: Run this command as a normal user, with sudo access"
+    exit 127
+fi
+loc="/tmp/jayan_setupScreenRecord"
+web_loc="sessions"
+mkdir ${loc}
+cd ${loc}
+sudo yum -y install httpd
+mkdir /var/www/html/${web_loc}
+git clone git@github.com:ysangkok/terminal_web_player.git
+cp terminal_web_player/view.* /var/www/html/${web_loc}
+wget https://raw.github.com/chjj/tty.js/d379c6f9/static/term.js -o /var/www/html/view/term.js
+mkdir -p ~/workshop/{bin,sessions}
+grep 'PATH=$PATH:~/workshop/bin' ~/.bashrc || echo "export PATH=$PATH:~/workshop/bin" >> ~/.bashrc
+
+systemctl enable httpd
+systemctl start httpd
